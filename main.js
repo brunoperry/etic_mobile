07465d19b6a273f687f978cf70a8807a7f79ad70
Bruno Perry <brunoperry@gmail.com>
2023-02-26 23:56:32 +0000

    updated class13

--- main.js
+++ main.js
@@ -38,10 +38,6 @@ const initialize = async (api_url) => {
         type: "reset",
         name: "reset",
       },
-      {
-        type: "exit",
-        name: "exit",
-      },
     ];
   } catch (error) {
     console.log(error);
