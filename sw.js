995a1bb6101e9ec4b3ee60eef9954cc72ae473be
Bruno Perry <brunoperry@gmail.com>
2023-03-08 15:27:49 +0000

    updated class 15

--- sw.js
+++ sw.js
@@ -1,9 +1,6 @@
-const VERSION = "1.0.0";
-const CACHE_NAME = "musicplayer-cache";
-
-// Define an array of files to cache
-const urlsToCache = [
-  "/",
+const VERSION = 1;
+const CACHE_NAME = "musicplayer";
+const cachedAssets = [
   "/index.html",
   "/app_logo.svg",
   "/manifest.json",
@@ -15,41 +12,28 @@ const urlsToCache = [
   "/images/screenshot2.png",
 ];
 
-// Install event listener
-self.addEventListener("install", async (event) => {
-  try {
+self.addEventListener("install", (installEvent) => {
+  console.log("install sw");
+  const response = async () => {
     const cache = await caches.open(CACHE_NAME);
-    await cache.addAll(urlsToCache);
-    await self.skipWaiting();
-
-    // Store the current version number in the cache
-    await cache.put("version", new Response(VERSION));
-  } catch (error) {
-    console.log("error", error);
-  }
+    cache.addAll(cachedAssets);
+  };
+  installEvent.waitUntil(response);
 });
 
-// Activate event listener
-self.addEventListener("activate", async (event) => {
-  // // Check the version number stored in the cache
-  const cache = await caches.open(CACHE_NAME);
-  const cachedVersion = await cache.match("version");
-  const currentVersion = new Response(VERSION);
-
-  if (!cachedVersion || cachedVersion !== currentVersion) {
-    // If the version numbers don't match, reload the page
-    await clients.claim();
-    // const clis = await self.clients.matchAll({ type: "window" }).then((clients) => {
-    //   clients.forEach((client) => client.navigate(client.url));
-    // });
-    const clis = await self.clients.matchAll({ type: "window" });
-    clis.forEach((cli) => cli.navigate(cli.url));
-  }
+self.addEventListener("activate", (event) => {
+  console.log("activate sw");
+  const response = async () => {
+    if ("navigationPreload" in self.registration) {
+      await self.registration.navigationPreload.enable();
+    }
+  };
+  event.waitUntil(response);
+  self.clients.claim();
 });
-
-// Fetch event listener
-self.addEventListener("fetch", async (event) => {
-  const responsePromise = (async () => {
+self.addEventListener("fetch", (event) => {
+  console.log("fetch sw");
+  const response = (async () => {
     try {
       const cachedResponse = await caches.match(event.request);
       return cachedResponse || fetch(event.request);
@@ -57,5 +41,5 @@ self.addEventListener("fetch", async (event) => {
       console.log("error fetching", error);
     }
   })();
-  event.respondWith(responsePromise);
+  event.respondWith(response);
 });
