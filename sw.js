f5af1c2c4eb473d942f249d77d60939bb358b82c
Bruno Perry <brunoperry@gmail.com>
2023-03-08 14:52:11 +0000

    updated class 15

--- sw.js
+++ sw.js
@@ -4,35 +4,34 @@ const CACHE_NAME = "musicplayer-cache";
 // Define an array of files to cache
 const urlsToCache = [
   "/",
-  "index.html",
-  "app_logo.svg",
-  "webmanifest.json",
-  "images/icons-192.png",
-  "images/icons-512.png",
-  "images/icons-vector.svg",
-  "images/maskable_icon.png",
-  "images/screenshot1.png",
-  "images/screenshot2.png",
+  "/index.html",
+  "/app_logo.svg",
+  "/manifest.json",
+  "/images/icons-192.png",
+  "/images/icons-512.png",
+  "/images/icons-vector.svg",
+  "/images/maskable_icon.png",
+  "/images/screenshot1.png",
+  "/images/screenshot2.png",
 ];
 
 // Install event listener
 self.addEventListener("install", async (event) => {
-  const cache = await caches.open(CACHE_NAME);
-  await cache.addAll(urlsToCache);
-  await self.skipWaiting();
+  try {
+    const cache = await caches.open(CACHE_NAME);
+    await cache.addAll(urlsToCache);
+    await self.skipWaiting();
 
-  // Store the current version number in the cache
-  await cache.put("version", new Response(VERSION));
+    // Store the current version number in the cache
+    await cache.put("version", new Response(VERSION));
+  } catch (error) {
+    console.log("error", error);
+  }
 });
 
 // Activate event listener
 self.addEventListener("activate", async (event) => {
-  const cacheNames = await caches.keys();
-  await Promise.all(
-    cacheNames.filter((name) => name !== CACHE_NAME).map((name) => caches.delete(name))
-  );
-
-  // Check the version number stored in the cache
+  // // Check the version number stored in the cache
   const cache = await caches.open(CACHE_NAME);
   const cachedVersion = await cache.match("version");
   const currentVersion = new Response(VERSION);
@@ -40,9 +39,11 @@ self.addEventListener("activate", async (event) => {
   if (!cachedVersion || cachedVersion !== currentVersion) {
     // If the version numbers don't match, reload the page
     await clients.claim();
-    await self.clients.matchAll({ type: "window" }).then((clients) => {
-      clients.forEach((client) => client.navigate(client.url));
-    });
+    // const clis = await self.clients.matchAll({ type: "window" }).then((clients) => {
+    //   clients.forEach((client) => client.navigate(client.url));
+    // });
+    const clis = await self.clients.matchAll({ type: "window" });
+    clis.forEach((cli) => cli.navigate(cli.url));
   }
 });
 
